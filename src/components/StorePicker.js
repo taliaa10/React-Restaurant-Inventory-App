691be6fac4af8cc08523f6a22125860b6625404d
Taliaa <taliaa10@ymail.com>
2019-09-15 13:51:06 -0500

    init commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <form action="" className="store-selector">
+          <h2>Please enter a store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store &rarr;</button>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default StorePicker;
